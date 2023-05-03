6b609403d73e0be5ef20b9e0333e87f96c9be953
Misael Guerrero <mguerrerowest@gmail.com>
2023-05-03 14:16:38 -0400

    Initiate Tree factory function

--- bst.js
+++ bst.js
@@ -1,7 +1,11 @@
-class node {
+class Node {
     constructor(data){
         this.data = data
         this.right = null
         this.left = null
     }
+}
+
+const Tree = (array) => {
+    root = null
 }
