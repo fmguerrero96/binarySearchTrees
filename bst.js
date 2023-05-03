331f9eb8d1c7b5ea72998621c4ecbe7c815f359e
Misael Guerrero <mguerrerowest@gmail.com>
2023-05-03 14:59:28 -0400

    create buildTree function which turns an array into a balanced BST

--- bst.js
+++ bst.js
@@ -8,4 +8,16 @@ class Node {
 
 const Tree = (array) => {
     root = null
+
+    const buildTreeRec = (arr) => {
+        //base case
+        if (start > end) {return null}
+
+        let mid = Math.floor((arr.length) / 2)  //find midPoint, which will then become the 'Root Node'
+        let rootNode = Node(arr[mid])
+        newNode.left = buildTreeRec(arr.slice(0,mid))  //set the left child recursively
+        newNode.right = buildTreeRec(arr.slice(mid+1, arr.length))  //set the right child recursively
+        root = rootNode
+        return root
+    }
 }
